fea925aba3b82fd30f6c1f5231b610db8b92b274
Takao Kodai (Lab Desktop) <takaokodai@team-lab,com>
2018-03-14 16:46:59 +0900

    デバッグGUIの共通部分をベースクラスに抜き出した

---
+++ Assets/Packages/PsuedoEnvironment/OSCEnvironment/Scripts/OSCDebugMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace OSCEnvironment
+{
+    public class OSCDebugMenu : DebugMenu
+    {
+
+        protected override void Start()
+        {
+
+            base.Start();
+
+            folds.Add("OSC", () =>
+            {
+                OSCManager.Instance.DebugMenuGUI();
+            });
+
+            folds.Add("Manager", () =>
+            {
+                EnvironmentManager.Instance.DebugMenuGUI();
+            });
+        }
+    }
+}
---
+++ Assets/Packages/PsuedoEnvironment/WebSocketEnvironment/Scripts/WebSocketDebugMenu.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace WebSocketEnvironment
+{
+    public class WebSocketDebugMenu : DebugMenu
+    {
+        
+        protected override void Start()
+        {
+
+            base.Start();
+
+            // 本来であればこれはコンテンツ側 (or Webサーバ) にあるべし
+            folds.Add("WebSocket Server", () =>
+            {
+                Server.Instance.DebugMenuGUI();
+            });
+
+            folds.Add("WebSocket Client", () =>
+            {
+
+                WebSocketManager.Instance.DebugMenuGUI();
+
+            });
+
+            folds.Add("Manager", () =>
+            {
+                EnvironmentManager.Instance.DebugMenuGUI();
+
+            });
+        }
+
+        
+
+    }
+}
